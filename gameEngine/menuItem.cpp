3d6af43d4f6af00e8b58d4f8efe60409c64961d0
Mahdi Romman <10613825@student.bpc.ac.uk>
2018-01-30 13:07:15 +0000

    Made it work again Commented out the parts for having an object of the object type in menuItem so the program can run again.
    Added codeblock files to the .gitignore.

--- gameEngine/menuItem.cpp
+++ gameEngine/menuItem.cpp
@@ -1,14 +1,14 @@
 #include "menuItem.h"
 
-menuItem::menuItem(int x, int y, int w, int h): object(w, h, false)
+menuItem::menuItem(int x, int y, int w, int h)//: mObject(w, h, false)
 {
 	rect.x = x;
 	rect.y = y;
 	rect.w = w;
 	rect.h = h;
 
-	object.rect.x = x;
-	object.rect.y = y;
+	//mObject.rect.x = x;
+	//mObject.rect.y = y;
 }
 
 menuItem::~menuItem()
--- gameEngine/menuItem.h
+++ gameEngine/menuItem.h
@@ -4,12 +4,14 @@
 class menuItem
 {
 public:
+
+    //object mObject;
 	menuItem(int x, int y, int w, int h);
 	~menuItem();
 
 	SDL_Rect rect;
 
-	object object;
+
 private:
 
 };
