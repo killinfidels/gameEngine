d766fde4a4103b04385a650aabc3354cb421f226
killinfidels <mahdi0800@gmail.com>
2018-02-09 14:26:32 +0000

    made eventHandler and pressed
    eventhandler records whether the mouse is over the object and if it has clicked
    pressed returns true if the mouse is over the object and has been clicked

--- gameEngine/menuItem.cpp
+++ gameEngine/menuItem.cpp
@@ -9,6 +9,40 @@ MenuItem::MenuItem(int x, int y, int w, int h) : Object(w, h, false)
 	//mObject.rect.y = y;
 }
 
+void MenuItem::eventHandler(SDL_Event* e)
+{
+//	SDL_MOUSEBUTTONDOWN
+//SDL_MOUSEBUTTONUP
+	switch(e->type)
+	{
+	case MOUSEBUTTONDOWN:
+		clickStatus = MOUSECLICK
+		break;
+	case MOUSEMOTION:
+		mouseX = e->motion.x;
+		mouseY = e->motion.y;
+		break;
+	default:
+		clickStatus = MOUSENOTHING;
+		break;
+	}
+	
+	if (!(mouseX > rect.x && mouseX < rect.x + rect.w))
+		locationStatus = MOUSENOTHING;
+	else if (!(mouseY > rect.y && mouseY < rect.y + rect.h))
+		locationStatus = MOUSENOTHING;
+	else
+		locationStatus = MOUSEOVER;
+				
+}
+
+bool MenuItem::pressed()
+{
+	if (clickStatus == MOUSECLICK && locationStatus)
+		return true;
+	return false;
+}
+
 MenuItem::~MenuItem()
 {
 }
